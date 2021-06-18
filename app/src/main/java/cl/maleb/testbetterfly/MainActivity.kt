8792dc80b8a3b6af735444c2e9f47566b68b7296
maleb1337 <pe.tobar.m@gmail.com>
2021-06-17 20:40:29 -0400

    Initial commit

---
+++ app/src/main/java/cl/maleb/testbetterfly/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.maleb.testbetterfly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
